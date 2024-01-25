f60c149cd38c603a5cfee9d6a4371eff66703677
Hung-Han (Henry) Chen <chenhungh@gmail.com>
2024-01-25 19:38:48 +0200

    Update README, add tests
    Signed-off-by: Hung-Han (Henry) Chen <chenhungh@gmail.com>

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # jwks
 Fetch and parse JSON Web Key Set (JWKS)
+
+## Usage
+
+From a jwks url.
+```rust
+let jwks_url = "https://www.googleapis.com/oauth2/v3/certs";
+let jwks = Jwks::from_jwks_url(jwks_url).await.unwrap();
+```
+
+From a openid config url.
+```rust
+let openid_config_url = "https://accounts.google.com/.well-known/openid-configuration";
+let jwks = Jwks::from_oidc_url(openid_config_url).await.unwrap();
+```
+
+Use with [jsonwebtokn](https://github.com/Keats/jsonwebtoken) to validate a jwt
+```rust
+use jsonwebtoken::{decode, decode_header, Algorithm, TokenData, Validation};
+
+let jwt = "...base64-encoded-jwt...";
+
+// get the kid from jwt
+let header = decode_header(jwt).expect("jwt header should be decoded");
+let kid = header.kid.as_ref().expect("jwt header should have a kid");
+
+// get a jwk from jwks by kid
+let jwks_url = "https://www.googleapis.com/oauth2/v3/certs";
+let jwks = Jwks::from_jwks_url().await.unwrap();
+let jwk = jwks.keys.get(kid).expect("jwt refer to a unknown key id");
+
+let validation = Validation::default();
+let decoded_token: TokenData<Claims> = decode::<Claims>(jwt, &jwk.decoding_key, &validation).expect("jwt should be valid");
+```
--- src/lib.rs
+++ src/lib.rs
@@ -9,26 +9,26 @@ use jsonwebtoken::{
 };
 use serde::Deserialize;
 use thiserror::Error;
-use tracing::debug;
 
 /// A container for a set of JWT decoding keys.
 ///
 /// The container can be used to validate any JWT that identifies a known key
 /// through the `kid` attribute in the token's header.
 #[derive(Clone)]
+#[allow(dead_code)]
 pub struct Jwks {
     keys: HashMap<String, Jwk>,
 }
 
 #[derive(Deserialize)]
-struct OidcConfig {
+struct OIDCConfig {
     jwks_uri: String,
 }
 
 impl Jwks {
     /// # Arguments
-    /// * `oidc_url` - The url with OpenID configuration, e.g. https://server.com/.well-known/openid-configuration
-    pub async fn from_oidc_url(oidc_url: &str) -> Result<Self, JwksError> {
+    /// * `oidc_url` - The url with OpenID configuration, e.g. https://accounts.google.com/.well-known/openid-configuration
+    pub async fn from_oidc_url(oidc_url: impl Into<String>) -> Result<Self, JwksError> {
         Self::from_oidc_url_with_client(&reqwest::Client::default(), oidc_url).await
     }
 
@@ -36,14 +36,13 @@ impl Jwks {
     /// passing in a custom [`Client`][reqwest::Client].
     pub async fn from_oidc_url_with_client(
         client: &reqwest::Client,
-        oidc_url: &str,
+        oidc_url: impl Into<String>,
     ) -> Result<Self, JwksError> {
-        debug!(%oidc_url, "Fetching openid-configuration.");
         let oidc_config = client
-            .get(oidc_url)
+            .get(oidc_url.into())
             .send()
             .await?
-            .json::<OidcConfig>()
+            .json::<OIDCConfig>()
             .await?;
         let jwks_uri = oidc_config.jwks_uri;
 
@@ -51,19 +50,18 @@ impl Jwks {
     }
 
     /// # Arguments
-    /// * `jwks_url` - The url which JWKS info is pulled from, e.g. https://example.com/.well-known/jwks.json
-    pub async fn from_jwks_url(jwks_url: &str) -> Result<Self, JwksError> {
-        Self::from_jwks_url_with_client(&reqwest::Client::default(), jwks_url).await
+    /// * `jwks_url` - The url which JWKS info is pulled from, e.g. https://www.googleapis.com/oauth2/v3/certs
+    pub async fn from_jwks_url(jwks_url: impl Into<String>) -> Result<Self, JwksError> {
+        Self::from_jwks_url_with_client(&reqwest::Client::default(), jwks_url.into()).await
     }
 
     /// A version of [`from_jwks_url`][Self::from_jwks_url] that allows for
     /// passing in a custom [`Client`][reqwest::Client].
     pub async fn from_jwks_url_with_client(
         client: &reqwest::Client,
-        jwks_url: &str,
+        jwks_url: impl Into<String>,
     ) -> Result<Self, JwksError> {
-        debug!(%jwks_url, "Fetching JSON Web Key Set.");
-        let jwks: jwk::JwkSet = client.get(jwks_url).send().await?.json().await?;
+        let jwks: jwk::JwkSet = client.get(jwks_url.into()).send().await?.json().await?;
 
         let mut keys = HashMap::new();
         for jwk in jwks.keys {
@@ -138,6 +136,7 @@ impl Jwks {
 }
 
 #[derive(Clone)]
+#[allow(dead_code)]
 struct Jwk {
     decoding_key: DecodingKey,
 }
@@ -181,12 +180,11 @@ mod tests {
     use serde_json::json;
 
     use super::*;
+    use httpmock::prelude::*;
 
     #[tokio::test]
-    async fn can_fetch_and_parse_jwks() {
-        let mut server = mockito::Server::new();
-
-        let url = server.url();
+    async fn can_fetch_and_parse_jwks_from_jwks_url() {
+        let server = MockServer::start();
         let jwks_path = "/oauth2/v3/certs";
 
         // from https://www.googleapis.com/oauth2/v3/certs
@@ -211,20 +209,144 @@ mod tests {
           ]
         });
 
-        let _ = server
-            .mock("GET", jwks_path)
-            .with_status(200)
-            .with_header("content-type", "application/json")
-            .with_body(jwks.to_string())
-            .create_async()
-            .await;
+        let _ = server.mock(|when, then| {
+            when.method(GET).path(jwks_path);
+            then.status(200)
+                .header("content-type", "application/json")
+                .body(jwks.to_string());
+        });
 
-        let jwks_url = format!("{}{}", url, jwks_path);
+        let jwks_url = server.url(jwks_path);
         let jwks = Jwks::from_jwks_url(&jwks_url).await.unwrap();
         assert_eq!(jwks.keys.len(), 2);
-        
+
         // get keys by key id (kid)
-        _ = &jwks.keys.get("91413cf4fa0cb92a3c3f5a054509132c47660937").expect("key one should be found");
-        _ = &jwks.keys.get("1f40f0a8ef3d880978dc82f25c3ec317c6a5b781").expect("key two should be found");
+        _ = &jwks
+            .keys
+            .get("91413cf4fa0cb92a3c3f5a054509132c47660937")
+            .expect("key one should be found");
+        _ = &jwks
+            .keys
+            .get("1f40f0a8ef3d880978dc82f25c3ec317c6a5b781")
+            .expect("key two should be found");
+    }
+
+    #[tokio::test]
+    async fn can_fetch_and_parse_jwks_from_oidc_config_url() {
+        let oidc_server = MockServer::start();
+
+        let oidc_config_path = "/.well-known/openid-configuration";
+        let jwks_path = "/oauth2/v3/certs";
+        let jwks_url = oidc_server.url(jwks_path);
+
+        // from https://accounts.google.com/.well-known/openid-configuration
+        let oidc_config = json!({
+         "issuer": "https://accounts.google.com",
+         "authorization_endpoint": "https://accounts.google.com/o/oauth2/v2/auth",
+         "device_authorization_endpoint": "https://oauth2.googleapis.com/device/code",
+         "token_endpoint": "https://oauth2.googleapis.com/token",
+         "userinfo_endpoint": "https://openidconnect.googleapis.com/v1/userinfo",
+         "revocation_endpoint": "https://oauth2.googleapis.com/revoke",
+         "jwks_uri": jwks_url,
+         "response_types_supported": [
+          "code",
+          "token",
+          "id_token",
+          "code token",
+          "code id_token",
+          "token id_token",
+          "code token id_token",
+          "none"
+         ],
+         "subject_types_supported": [
+          "public"
+         ],
+         "id_token_signing_alg_values_supported": [
+          "RS256"
+         ],
+         "scopes_supported": [
+          "openid",
+          "email",
+          "profile"
+         ],
+         "token_endpoint_auth_methods_supported": [
+          "client_secret_post",
+          "client_secret_basic"
+         ],
+         "claims_supported": [
+          "aud",
+          "email",
+          "email_verified",
+          "exp",
+          "family_name",
+          "given_name",
+          "iat",
+          "iss",
+          "locale",
+          "name",
+          "picture",
+          "sub"
+         ],
+         "code_challenge_methods_supported": [
+          "plain",
+          "S256"
+         ],
+         "grant_types_supported": [
+          "authorization_code",
+          "refresh_token",
+          "urn:ietf:params:oauth:grant-type:device_code",
+          "urn:ietf:params:oauth:grant-type:jwt-bearer"
+         ]
+        });
+
+        let _ = oidc_server.mock(|when, then| {
+            when.method(GET).path(oidc_config_path);
+            then.status(200)
+                .header("content-type", "application/json")
+                .body(oidc_config.to_string());
+        });
+
+        // from https://www.googleapis.com/oauth2/v3/certs
+        let jwks = json!({
+          "keys": [
+            {
+              "use": "sig",
+              "n": "jb1Ps3fdt0oPYPbQlfZqKkCXrM1qJ5EkfBHSMrPXPzh9QLwa43WCLEdrTcf5vI8cNwbgSxDlCDS2BzHQC0hYPwFkJaD6y6NIIcwdSMcKlQPwk4-sqJbz55_gyUWjifcpXXKbXDdnd2QzSE2YipareOPJaBs3Ybuvf_EePnYoKEhXNeGm_T3546A56uOV2mNEe6e-RaIa76i8kcx_8JP3FjqxZSWRrmGYwZJhTGbeY5pfOS6v_EYpA4Up1kZANWReeC3mgh3O78f5nKEDxwPf99bIQ22fIC2779HbfzO-ybqR_EJ0zv8LlqfT7dMjZs25LH8Jw5wGWjP_9efP8emTOw",
+              "kty": "RSA",
+              "alg": "RS256",
+              "e": "AQAB",
+              "kid": "91413cf4fa0cb92a3c3f5a054509132c47660937"
+            },
+            {
+              "n": "tgkwz0K80MycaI2Dz_jHkErJ_IHUPTlx4LR_6wltAHQW_ZwhMzINNH8vbWo8P5F2YLDiIbuslF9y7Q3izsPX3XWQyt6LI8ZT4gmGXQBumYMKx2VtbmTYIysKY8AY7x5UCDO-oaAcBuKQvWc5E31kXm6d6vfaEZjrMc_KT3DsFdN0LcAkB-Q9oYcVl7YEgAN849ROKUs6onf7eukj1PHwDzIBgA9AExJaKen0wITvxQv3H_BRXB7m6hFkLbK5Jo18gl3UxJ7Em29peEwi8Psn7MuI7CwhFNchKhjZM9eaMX27tpDPqR15-I6CA5Zf94rabUGWYph5cFXKWPPr8dskQQ",
+              "alg": "RS256",
+              "use": "sig",
+              "kid": "1f40f0a8ef3d880978dc82f25c3ec317c6a5b781",
+              "e": "AQAB",
+              "kty": "RSA"
+            }
+          ]
+        });
+
+        let _ = oidc_server.mock(|when, then| {
+            when.method(GET).path(jwks_path);
+            then.status(200)
+                .header("content-type", "application/json")
+                .body(jwks.to_string());
+        });
+
+        let oidc_config_url = oidc_server.url(oidc_config_path);
+        let jwks = Jwks::from_oidc_url(&oidc_config_url).await.unwrap();
+        assert_eq!(jwks.keys.len(), 2);
+
+        // get keys by key id (kid)
+        _ = &jwks
+            .keys
+            .get("91413cf4fa0cb92a3c3f5a054509132c47660937")
+            .expect("key one should be found");
+        _ = &jwks
+            .keys
+            .get("1f40f0a8ef3d880978dc82f25c3ec317c6a5b781")
+            .expect("key two should be found");
     }
 }
